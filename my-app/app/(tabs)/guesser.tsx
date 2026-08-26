f46c47f0bf9054f344f2e087ebd8abb3a0318527
Ella Ekström <eltsu.ekstrom@gmail.com>
2026-08-26 14:22:12 +0300

    add: stylesheet and title

--- my-app/app/(tabs)/guesser.tsx
+++ my-app/app/(tabs)/guesser.tsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function GuesserScreen() {
-    
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    
-})
+  container: {
+    backgroundColor: "pink",
+    borderWidth: 5,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  text: {},
+});
